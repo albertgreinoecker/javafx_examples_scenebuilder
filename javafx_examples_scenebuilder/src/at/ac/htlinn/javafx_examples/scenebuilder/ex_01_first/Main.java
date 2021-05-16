77e865486c6729e1f9ca235920a279d90f4356cc
Albert Greinoecker <albert.greinoecker@gmail.com>
2021-05-16 19:19:33 +0200

    add doc

--- javafx_examples_scenebuilder/src/at/ac/htlinn/javafx_examples/scenebuilder/ex_01_first/Main.java
+++ javafx_examples_scenebuilder/src/at/ac/htlinn/javafx_examples/scenebuilder/ex_01_first/Main.java
@@ -6,7 +6,9 @@ import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
 
-
+/**
+ * simple calculator with a listview
+ */
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
